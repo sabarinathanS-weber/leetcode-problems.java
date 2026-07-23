7083c97f30f42a5c957365b3f9689eb6309b9016
sabarinathanS-weber <sabarinathansankar07@gmail.com>
2026-07-23 11:13:21 +0530

    Implement method to determine chess board square color

---
+++ determine colour of a chess board.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        int col = coordinates.charAt(0) - 'a';
+        int row = coordinates.charAt(1) - '1';
+        return (col + row) % 2 == 1;
+    }
+}
